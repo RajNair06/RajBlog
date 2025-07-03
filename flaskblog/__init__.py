cfef35db8b64efcdc87752083cbf8cf059695265
Raj Nair <nraj00954@gmail.com>
2025-07-03 10:50:48 +0000

    back to sqlite and changed version of itsdangerous module

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
-
+from flask_migrate import Migrate
 
 
 
@@ -16,6 +16,7 @@ from flaskblog.config import Config
 
 
 db=SQLAlchemy()
+migrate=Migrate()
 bcrypt=Bcrypt()
 login_manager=LoginManager()
 login_manager.login_view='users.login'
@@ -30,6 +31,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app,db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
@@ -43,8 +45,7 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(errors)
 
-    with app.app_context():
-        db.create_all()
+
 
     return app
     
